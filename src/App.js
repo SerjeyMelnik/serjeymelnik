41fa9b8f472cbaf2697767c49071cae3434c5e2e
ast_smelnyk <s.melnyk@astoundcommerce.com>
2022-04-07 15:29:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function myfun() {
+    console.log(arguments);
+  }
   return (
     <div className="App">
       <header className="App-header">
